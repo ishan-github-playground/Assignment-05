059c71e457e6ed7739f8176bcf474ba7b13e11a8
Tharindu Ishan <ishanthari96@gmail.com>
2023-08-15 23:38:44 +0530

    3rd commit

--- Assignment01.java
+++ Assignment01.java
@@ -21,15 +21,15 @@ public class Assignment01{
         final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
         final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
 
-        String[] customerIds = new String[0];
-        String[] customerNames = new String[0];
+        String[] names = new String[0];
+        double[] deposits=new double[0];
 
         String screen = DASHBOARD;
-
+loop:
         do{
             final String APP_TITLE = String.format("%s%s%s",
             COLOR_BLUE_BOLD, screen, RESET);
-            System.out.println(CLEAR);
+            // System.out.println(CLEAR);
             System.out.println("\t" + APP_TITLE + "\n");
 
             switch(screen){
@@ -55,7 +55,76 @@ public class Assignment01{
                         case 7: System.out.println(CLEAR); System.exit(0);
                         default: continue; 
                     }
+                    break;
+                case OPEN_ACCOUNT:
+                    System.out.printf("\tID: SDB-%05d \n", (names.length + 1));
+
+                    boolean valid;
+                    String name;
+
+                    do{
+                        valid=true;
+                        System.out.print("\tName:");
+                        name=scanner.nextLine().strip();
+                        if(name.isEmpty()){
+                            System.out.printf(ERROR_MSG,"Name Can't be empty");
+                            valid=false;
+                            continue;
+                        }
+                        for (int i = 0; i < name.length(); i++) {
+                            if (!(Character.isLetter(name.charAt(i)) || 
+                                Character.isSpaceChar(name.charAt(i))) ) {
+                                System.out.printf("\t%sInvalid Name%s\n", COLOR_RED_BOLD, RESET);
+                                valid = false;
+                                break;
+                            }
+                        }
+                    }while(!valid);
+
                     
+
+                    double deposit;
+                    do{
+                        valid=true;
+                        System.out.print("\tInitial Deposit:");
+                        deposit=scanner.nextDouble();
+                        scanner.nextLine();
+                        if(deposit<5000){
+                            System.out.printf(ERROR_MSG,"Insuficient Amount Please Deposite more than Rs.5000.00");
+                            valid=false;
+                            continue;
+                        }
+
+
+                    }while(!valid);
+
+                    String[] newNames=new String[names.length+1];
+                    for (int i = 0; i < names.length; i++) {
+                        newNames[i]=names[i];
+                    }
+                    newNames[newNames.length-1]=name;
+                    names=newNames;
+
+                    double[] newDeposits=new double[deposits.length+1];
+                    for (int i = 0; i < deposits.length; i++) {
+                        newDeposits[i]=deposits[i];
+                    }
+                    newDeposits[newDeposits.length-1]=deposit;
+                    deposits=newDeposits;
+
+                    System.out.println();
+                    System.out.printf(SUCCESS_MSG,"\t" + name + " your account created sucessfully.\n\tDo you want to create new Account (Y/n)? ");
+                    if(scanner.nextLine().strip().toUpperCase().equals("Y"))continue;
+                    screen=DASHBOARD;
+                    break;
+                    
+                case DEPOSIT:
+                case WITHDRAW:
+                case TRANSFER:
+                case CHECK_BALANCE:
+                case DROP_ACCOUNT:
+
+
 
 
 
